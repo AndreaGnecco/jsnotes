92b073e43d53ca50856356a05fbb95f8187c7f41
Andrea Gnecco <andreagnecco@gmail.com>
2018-01-19 17:09:16 -0200

    Função funcionando

--- js/javascript.js
+++ js/javascript.js
@@ -1,7 +1,13 @@
-objItem {
-    
-}
-
 function addItem(){
+    var label = document.createElement("span");
+    document.getElementById("items").appendChild(label);
+
+    label.className = "cadastro-notas__inside--label";
+
+    var input = document.createElement("input");
+    document.getElementById("items").appendChild(input);
+
+    input.className = "cadastro-notas__inside--input";
+
     
 }
