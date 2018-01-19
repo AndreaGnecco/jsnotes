677dbabcc040a78f3d5ad81b589b4f2d0ffd82f5
andreagnecco <andreagnecco@gmail.com>
2018-01-19 21:52:27 -0200

    Finalizado html e css

--- js/javascript.js
+++ js/javascript.js
@@ -2,8 +2,17 @@ var item = [];
 
 
 function addItem() {
+    var innerHTML = "";
 
-
+    for (var index = 0; index < item.length; index++) {
+     innerHTML += '<div class = "cadastro-notas__inside--tudo" id="items">' + 
+                    '<label class="cadastro-notas__inside--label">ITEM' + index + '</label>' +
+                    '<input type="text" name="item" id="notas" class="cadastro-notas__inside--input">' +
+                    '<input type="button" id="btnDel" value="-" onclick="deleteItem()" class="cadastro-notas__inside--add-delete">' +
+                    '</div>';
+    
+    
+    
     }
 
 
