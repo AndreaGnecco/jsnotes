d55e46a9d3a0cec2b7c1ec832339cef25aa6a1d6
ANDREA GNECCO <06067377900@jpmorgan.sp>
2018-01-24 21:52:47 -0200

    Terceiro

--- js/javascript.js
+++ js/javascript.js
@@ -1,20 +1,18 @@
-var item = [];
-
-
-function addItem() {
-    var innerHTML = "";
-
-    for (var index = 0; index < item.length; index++) {
-     innerHTML += '<div class = "cadastro-notas__inside--tudo" id="items">' + 
-                    '<label class="cadastro-notas__inside--label">ITEM' + index + '</label>' +
-                    '<input type="text" name="item" id="notas" class="cadastro-notas__inside--input">' +
-                    '<input type="button" id="btnDel" value="-" onclick="deleteItem()" class="cadastro-notas__inside--add-delete">' +
-                    '</div>';
-    
-    
-    
-    }
+//adicionar input de item
 
 
 
+function addItem(div) {
+    var input = [];
+    var cadastroInput = "";
 
+    for (var i = 0; i < input.length; i++) {
+        cadastroInput += '<div class="cadastro-notas__inside--tudo" id="items">' +
+                '<label class="cadastro-notas__inside--label">' + 'ITEM ' + i + '</label>' +
+                '<input type="text" name="item" id="notas" class="cadastro-notas__inside--input">' +
+                '<input type="button" id="btnDel" value="-" onclick="deleteItem()" class="cadastro-notas__inside--add-delete">' +
+            '</div>';
+        }
+        
+    div.innerHTML = cadastroInput;
+}
