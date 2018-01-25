6abda8b1ada3f1e8543b20cd500a7d6c4fd7525d
andreagnecco <andreagnecco@gmail.com>
2018-01-25 15:38:24 -0200

    mudanças no html e css

--- js/javascript.js
+++ js/javascript.js
@@ -1,18 +1,5 @@
 //adicionar input de item
 
-
-
-function addItem(div) {
-    var input = [];
-    var cadastroInput = "";
-
-    for (var i = 0; i < input.length; i++) {
-        cadastroInput += '<div class="cadastro-notas__inside--tudo" id="items">' +
-                '<label class="cadastro-notas__inside--label">' + 'ITEM ' + i + '</label>' +
-                '<input type="text" name="item" id="notas" class="cadastro-notas__inside--input">' +
-                '<input type="button" id="btnDel" value="-" onclick="deleteItem()" class="cadastro-notas__inside--add-delete">' +
-            '</div>';
-        }
-        
-    div.innerHTML = cadastroInput;
+function addInput(div) {
+    
 }
