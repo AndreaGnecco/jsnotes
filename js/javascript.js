38515e6bfb16206e26be45d3d54ddf86587674a8
andreagnecco <andreagnecco@gmail.com>
2018-02-01 14:14:38 -0200

    =(

--- js/javascript.js
+++ js/javascript.js
@@ -1,5 +1,16 @@
 //adicionar input de item
-
-function addInput(div) {
+var items = '';
+function addInput(items) {
+    var novoItem = '';
     
+    for (contador = 1; contador < items.length; contador++) {
+        novoItem += '<form class="item">' +
+                    '<label class="item-label">ITEM ' + contador + '</label>' +
+                    '<input class="item-input" type="text">' +
+                    '<input class="item-button" type="button" value="-" onclick="">' +
+                '</form> '
+    }
+
+    items.innerHTML = novoItem;
+
 }
