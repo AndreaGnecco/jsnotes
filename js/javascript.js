7fe2b800fbfe457dbb9440236de609a2c1a30c08
andreagnecco <andreagnecco@gmail.com>
2018-02-01 16:40:19 -0200

    add itens funcionando

--- js/javascript.js
+++ js/javascript.js
@@ -1,16 +1,18 @@
-//adicionar input de item
-var items = '';
-function addInput(items) {
+//adicionar input de item 
+secao = document.getElementById("novo-item");
+
+function addInput() {
+
     var novoItem = '';
-    
-    for (contador = 1; contador < items.length; contador++) {
+
+    for (contador = 1; contador <= secao.childElementCount + 1; contador++) {
         novoItem += '<form class="item">' +
-                    '<label class="item-label">ITEM ' + contador + '</label>' +
+                    '<label class="item-label">' + 'ITEM ' + contador + '</label>' +
                     '<input class="item-input" type="text">' +
                     '<input class="item-button" type="button" value="-" onclick="">' +
                 '</form> '
     }
 
-    items.innerHTML = novoItem;
-
+    
+    secao.innerHTML = novoItem;
 }
